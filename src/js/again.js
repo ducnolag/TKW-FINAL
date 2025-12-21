b9829be2d090ea7881c6d6345702c036d7fb758e
ducnolag <duchaiphong97@gmail.com>
2025-12-21 19:20:01 +0700

    up thu

--- src/js/again.js
+++ src/js/again.js
@@ -4,3 +4,8 @@ import './layouts/floating-navigation.js'
 import './layouts/header.js';
 import './layouts/chatbot.js';
 import './layouts/footer.js';
+
+
+fetch('/data/product.json')
+    .then(res => res.json())
+    .then(data => localStorage.setItem('products_data', JSON.stringify(data)));
